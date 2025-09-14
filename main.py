4e93aa84ee15e8820e50693263306753bf86f9cd
adnandzul <adnandzu06@gmail.com>
2025-09-14 16:12:24 +0700

    commit perubahan

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World! Otomatis deploy dari GitHub Actions berhasil!"}
+    # Pesan ini akan kita ubah nanti untuk tes deploy
+    return {"message": "Hello World! v1.0"}
 
-@app.get("/status")
-def get_status():
-    return {"status": "ok"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
